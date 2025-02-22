e3fdb08291191008efb3e7d89627fd1ecf279654
Raynara <raynara.gustavo@escolar.ifrn.edu.br>
2025-02-22 20:20:14 -0300

    footer? eu acho...

--- projeto_2/src/app/app.component.ts
+++ projeto_2/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./_components/header/header.component";
+import { FooterComponent } from "./_components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
